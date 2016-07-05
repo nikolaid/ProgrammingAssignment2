78814b606e9185fa3f3db91dedac3a098bd7ef56
Christos N <c.nikolaid@gmail.com>
2016-07-05 14:10:50 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m<-NULL
+set <- function(y) {
+x <- y
+m <- NULL
 }
+get <- function() x
+setinverse <- function(Solve) m <- Solve
+getinverse <- function() m
+list(set = set, get = get,setinverse = setinverse,getinverse = getinverse)}
 
 
 ## Write a short comment describing this function
